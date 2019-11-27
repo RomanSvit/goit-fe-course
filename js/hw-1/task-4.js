01b92f82ad6c819391cefb95b6628622cc768c5d
RomanSvit <roman.svitelskyi@gmail.com>
2019-11-27 17:34:36 +0200

    changes 4-file

---
+++ js/hw-1/task-4.js
@@ -0,0 +1,19 @@
+'use strict';
+const credits = 23580;
+const pricePerDroid = 3000;
+const order = prompt('Введите количество дроидов.');
+let totalPrice = order * pricePerDroid;
+let oddMoney = credits - totalPrice;
+let message;
+if (order === null) {
+  message = 'Отменено пользователем!';
+} else if (credits < totalPrice) {
+  message = 'Недостаточно средств на счету!';
+}else if(order === ''){
+  message = 'Вы не ввели количество товара!';
+} else if (credits >= totalPrice) {
+  message = `Вы купили ${order} дроидов, на счету осталось ${oddMoney} кредитов.`;
+} else if (order > 0) {
+  message = totalPrice;
+} 
+console.log(message);
