47bbbf90b3c7da921045342481cff133681a776a
Roman Svitelskyi <roman.svitelskyi@gmail.com>
2019-12-01 09:59:58 +0200

    changes

--- js/hw-2/task1.js
+++ js/hw-2/task1.js
@@ -1,4 +1,10 @@
 'user strict';
-function task1(){
-  
+function task1() {
+  let numberElement =
+  const logItems = function (array) {
+    for (const arrayItems of array) {
+      console.log(arrayItems);
+    }
+
+  }
 }
