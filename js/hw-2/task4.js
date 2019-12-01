29d10d3006e2153402fee528c232574d8d17ff41
Roman Svitelskyi <roman.svitelskyi@gmail.com>
2019-12-01 19:59:29 +0200

    changes in task4

--- js/hw-2/task4.js
+++ js/hw-2/task4.js
@@ -1,4 +1,21 @@
 'user strict';
-function task4(){
-  
+function task4() {
+  const formatString = (string) => {
+    let lengthString = string.length;
+    let sliceString = string.slice(0, 39);
+    const toBeContinued = '...';
+    let shortString;
+    for (let i = 0; i < string.length; i += 1) {
+      if (lengthString < 40) {
+        shortString = string;
+      } else {
+        shortString = `${sliceString}${toBeContinued}`;
+      }
+    }
+    return shortString;
+  }
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  console.log(formatString('Curabitur ligula sapien.'));
+  console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
 }
