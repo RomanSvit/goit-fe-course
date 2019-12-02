d4fcfae2287aa415feb5fdbdd8ce3e1e3a0a5da1
Roman Svitelskyi <roman.svitelskyi@gmail.com>
2019-12-02 15:17:14 +0200

    changes

--- js/hw-3/task1.js
+++ js/hw-3/task1.js
@@ -1,4 +1,20 @@
 'user strict';
-function task1(){
-  
+function task1() {
+  const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+  };
+  const user2 = {
+    mood: 'happy',
+    hobby: 'skydiving',
+    premium: false,
+  }
+  const newUser = Object.assign({}, user, user2);
+  const arrayNewUser = Object.keys(newUser);
+  for (const keys of arrayNewUser) {
+
+    console.log(`${keys}: ${newUser[keys]}`);
+  }
 }
--- js/hw-3/task2.js
+++ js/hw-3/task2.js
@@ -1,4 +1,9 @@
 'user strict';
 function task2() {
-
+  const countProps = (obj) => {
+    return Object.keys(obj).length;
+  }
+  console.log(countProps({})); // 0
+  console.log(countProps({ name: 'Mango', age: 2 })); // 2
+  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
 }
