129f3febbec9ad755ee882af6a1199937b32d5c0
Roman Svitelskyi <roman.svitelskyi@gmail.com>
2019-12-02 20:03:09 +0200

    changes

--- js/hw-3/task4.js
+++ js/hw-3/task4.js
@@ -1,4 +1,19 @@
 'user strict';
 function task4() {
-
+  const countTotalSalary = (employees) => {
+    let totalSalary = 0;
+    for (const key in employees) {
+      totalSalary += employees[key];
+    } console.log(totalSalary);
+  }
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  });
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  });
 }
